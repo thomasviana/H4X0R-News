c45b460367a4850487ecb95a3b85aa2359041627
Thomas Viana <thomasviana16@gmail.com>
2021-09-19 20:16:43 -0500

    Networking with JSONDecoder and implementing Observer Design Pattern

--- H4X0R News/Views/ContentView.swift
+++ H4X0R News/Views/ContentView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
+    @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationTitle("H4X0R News")
         }
+        .onAppear(perform: {
+            self.networkManager.fetchData()
+        })
     }
 }
 
@@ -26,14 +32,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts =  [
-    Post(id: "1", title: "Hello1"),
-    Post(id: "2", title: "Hello2"),
-    Post(id: "3", title: "Hello3")
-]
